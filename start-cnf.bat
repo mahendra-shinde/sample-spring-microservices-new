89cc6f4d6b360f804ea3d2338ccb4fbc6ef2391e
Mahendra Shinde <MahendraShinde@synegertics-india.com>
2024-07-17 21:58:55 +0530

    Update-Spring-Boot-3.0

---
+++ start-cnf.bat
@@ -0,0 +1,1 @@
+start "Config Server" java -jar config-service\target\config-service-1.0-SNAPSHOT.jar
---
+++ start-config.ps1
@@ -0,0 +1,9 @@
+$APP=@{CNFSRV ="config-service\target\config-service-1.0-SNAPSHOT.jar";
+DISSRV ="discovery-service\target\discovery-service-1.0-SNAPSHOT.jar";
+DEPSRV ="department-service\target\department-service-1.1.jar";
+EMPSRV ="employee-service\target\employee-service-1.1.jar";
+ORGSRV ="organization-service\target\organization-service-1.1.jar";
+GWYSRV ="gateway-service\target\gateway-service-1.0-SNAPSHOT.jar"
+}
+
+
