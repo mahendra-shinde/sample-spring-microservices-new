12648ced7260cf358bc02f0ea07f3ca2ef89e474
Mahendra Shinde <MahendraShinde@synegertics-india.com>
2024-07-18 09:54:59 +0530

    Fixed Organization and Proxy services

--- proxy-service/src/main/java/pl/piomin/service/proxy/api/ProxyApi.java
+++ proxy-service/src/main/java/pl/piomin/service/proxy/api/ProxyApi.java
@@ -1,16 +1,10 @@
 package pl.piomin.service.proxy.api;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
-import springfox.documentation.swagger.web.SwaggerResource;
-import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class ProxyApi {
@@ -18,23 +12,5 @@ public class ProxyApi {
 	@Autowired
 	ZuulProperties properties;
 
-	@Primary
-	@Bean
-	public SwaggerResourcesProvider swaggerResourcesProvider() {
-		return () -> {
-			List<SwaggerResource> resources = new ArrayList<>();
-			properties.getRoutes().values().stream()
-					.forEach(route -> resources.add(createResource(route.getServiceId(), route.getId(), "2.0")));
-			return resources;
-		};
-	}
-
-	private SwaggerResource createResource(String name, String location, String version) {
-		SwaggerResource swaggerResource = new SwaggerResource();
-		swaggerResource.setName(name);
-		swaggerResource.setLocation("/" + location + "/v2/api-docs");
-		swaggerResource.setSwaggerVersion(version);
-		return swaggerResource;
-	}
 
 }
